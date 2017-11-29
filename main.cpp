50f59ee0926f7b870af57a37a2708c9774fe39b3
git-snpy <iyemon.500ml@gmail.com>
2017-11-29 13:20:31 +0900

    add new Func2, 3, 4

--- main.cpp
+++ main.cpp
@@ -1,26 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include <kv/allsol.hpp>
 
-/* 
- * File:   main.cpp
- * Author: inoue
- *
- * Created on November 29, 2017, 10:41 AM
- */
+namespace ub = boost::numeric::ublas;
 
-#include <cstdlib>
+struct Func {
+	template <class T> ub::vector<T> operator() (const ub::vector<T>& x){
+		ub::vector<T> y(2);
 
-using namespace std;
+		y(0) = x(0) * x(0) + x(1) * x(1) - 1;
+		y(1) = x(0) - x(1);
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-    
-    
-    return 0;
+		return y;
+	}
+};
+
+struct Func2 {
+	template <class T> ub::vector<T> operator() (const ub::vector<T>& x){
+		ub::vector<T> y(2);
+
+		y(0) = x(0) * x(0) + x(1) * x(1) - 1;
+		y(1) = 2 * x(0) * x(0) - x(1) - 1;
+
+		return y;
+	}
+};
+
+struct Func3 {
+	template <class T> T operator() (const T& x){
+		return x / 10 + sin(x);
+	}
+};
+
+struct Func4 {
+	template <class T> T operator() (const T& x){
+		return x*x*x - x*x;
+	}
+};
+
+
+typedef kv::interval<double> itv;
+
+int main()
+{
+	int i;
+	ub::vector<itv> I;
+
+	std::cout.precision(17);
+
+	I.resize(2);
+	for (i=0; i<I.size(); i++) I(i) = itv(.5, 1.);
+
+	kv::allsol(Func(), I);
+
+	kv::allsol(Func2(), I, 1, 1e-8);
+
+	kv::allsol(Func3(), itv(-10, 10));
+
+	kv::allsol(Func4(), itv(-10, 10), 1, 1e-8);
 }
-
