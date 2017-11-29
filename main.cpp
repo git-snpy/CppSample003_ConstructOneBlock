d223441f4cd4b3c3cfab15b6239d654f0488f225
git-snpy <iyemon.500ml@gmail.com>
2017-11-29 10:54:21 +0900

    initial setting

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: inoue
+ *
+ * Created on November 29, 2017, 10:41 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+    return 0;
+}
+
